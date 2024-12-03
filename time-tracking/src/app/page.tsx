31e4a81854ed8ec502108e9d761e89d6f9900811
chandeepshrma <chandeepsharma828@gmail.com>
2024-12-03 23:33:02 +0530

    basic-project-setup

---
+++ time-tracking/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Login from "@/components/login";
+
+export default function Home() {
+  return (
+   <>
+   <div className="flex">
+      <div className="w-18">
+    <Login/>
+      </div>
+   </div>
+   </>    
+  );
+}
---
+++ time-tracking/src/components/login.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Input } from './ui/input'
+
+export default function login() {
+  return (
+    <div>
+      <Input/>
+    </div>
+  )
+}
